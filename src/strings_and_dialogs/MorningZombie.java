7e25a04eb44ddf878077bb52e210fef796e0661a
11000783 <josephaguilar2022@gmail.com>
2018-12-01 11:10:18 -0800

    saved

---
+++ src/strings_and_dialogs/MorningZombie.java
@@ -0,0 +1,10 @@
+package strings_and_dialogs;
+
+public class MorningZombie {
+public static void main(String[] args) {
+	wake up, change cloths, brush teeth, eat, brush teeth, go somewhere.
+	
+JoptionPane.showme
+
+}
+}
--- src/strings_and_dialogs/RobotGraffiti.java
+++ src/strings_and_dialogs/RobotGraffiti.java
@@ -6,11 +6,19 @@ public class RobotGraffiti {
 
 	public static void main(String[] args) throws Exception {
 		Robot bee = new Robot();
+		bee.setSpeed(15);
 		bee.penDown();
 		bee.turn(25);
 		bee.move(200);
-		bee.turn(95);
+		bee.turn(125
+			);
 		bee.move(200);
+		bee.turn(180);
+		bee.move(70);
+		bee.turn(300);
+		bee.move(115);
+		bee.penUp();
+		bee.hide();
 	}
 
 }
